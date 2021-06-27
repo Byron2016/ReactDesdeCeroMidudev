2835c5f58f0609427bde51795c19842183d2cc77
Byron 2016 <bgva2005@yahoo.com>
2021-06-27 14:54:25 -0500

    V02_Añadiendo rutas con wouter (alternativa a react router dom) (1.05.50)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import {useState} from 'react'
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
 
+import { Route } from 'wouter'
+
 function App() {
   const [keyword, setKeyword] = useState('panda')
   return (
     <div className="App">
       <section className="App-content">
+        <h1>App</h1>
+        <a href='/gif/panda'>Gifs de pandas</a>
+        <a href='/gif/ecuador'>Gifs de Ecuador</a>
+        <a href='/gif/chile'>Gifs de Chile</a>
+        <Route 
+          component={ListOfGifs} 
+          path="/gif/:keyword" />
+        {/* 
         <button onClick={() => setKeyword('mapache')}>Cambiar keyword</button>
-        <ListOfGifs keyword={keyword} />
+        <ListOfGifs keyword={keyword} /> 
+        */}
       </section>
     </div>
   );
--- src/components/Gif.js
+++ src/components/Gif.js
@@ -2,10 +2,9 @@ import './Gif.css'
 
 export default function Gif({title, id, url}) {
   return (
-    <div className='Gif'>
+    <a href={`#${id}`} className='Gif'>
       <h4>{title}</h4>
-      <p><small>{id}</small></p>
       <img src={url} alt={title}/>
-    </div>
+    </a>
   )
 }
