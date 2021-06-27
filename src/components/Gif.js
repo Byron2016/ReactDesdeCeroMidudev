3c24df040a15f74e972fd126ed51720a4e0a0744
Byron 2016 <bgva2005@yahoo.com>
2021-06-27 13:01:01 -0500

    V02_Añadiendo algo de estilos (01.04.05)

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,6 +1,8 @@
+import './Gif.css'
+
 export default function Gif({title, id, url}) {
   return (
-    <div>
+    <div className='Gif'>
       <h4>{title}</h4>
       <p><small>{id}</small></p>
       <img src={url} alt={title}/>
