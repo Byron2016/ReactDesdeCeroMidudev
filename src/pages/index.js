5a725034575bc36de327687604e306310599bd6c
Byron 2016 <bgva2005@yahoo.com>
2021-06-27 19:40:23 -0500

    V03_Pagina Home index inicial (7.43)

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import {Link} from 'wouter'
+
+const POPULAR_GIFS = ["Matrix", "Venezuela", "Chile", "Ecuador"]
+
+export default function Home() {
+  return (
+    <>
+      <h3 className="App-title">Los gifs más populares</h3>
+      <ul>
+        {POPULAR_GIFS.map((popularGif) => (
+          <li>
+            <Link to={'/search/${popularGif}'}>Gifs de {popularGif}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
