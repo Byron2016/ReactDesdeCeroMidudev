2f2cb55b286fcf09310cde389c464363ef3d3a54
Byron 2016 <bgva2005@yahoo.com>
2021-06-27 21:00:57 -0500

    V03P_Paso componente Git a carpeta propia

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,29 +1,24 @@
 import {useEffect, useState} from 'react'
-import Gif from './Gif'
+import Gif from './Gif/index'
+//import Gif from './Gif'
 import getGifs from '../services/getGifs';
 
 export default function ListOfGifs({params}) {
   const {keyword} = params
   const [loading, setLoading] = useState(false)
   const [gifs, setGifs] = useState([])
-  // const [gifs, setGifs] = useState(
-  //   { loading: false, results:[]}
-  // )
-  // Fin V02
 
   useEffect(() => {
     setLoading(true)
-    // setGifs(actualGifs => ({loading: true, results: gifs.results}))
 
     getGifs({ keyword })
       .then(gifsRetornados => {
-        // setGifs({loading: false, results: gifsRetornados})
         setGifs(gifsRetornados)
         setLoading(false)
       })
   }, [ keyword])
 
-  if(gifs.loading) return <i>Cargando 🕘</i>
+  if(loading) return <i>Cargando 🕘</i>
   
   return <div>
     {
