c148c1d22c05d3e4e68b9b344e4b623120ea6c23
Byron 2016 <bgva2005@yahoo.com>
2021-06-27 19:59:24 -0500

    V03_Añadiendo un form que haga de buscador de Gifs y uso de useLocation de wouter (7.22)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,28 @@
-import {Link} from 'wouter'
+import { useState } from 'react'
+import {Link, useLocation} from 'wouter'
 
 const POPULAR_GIFS = ["Matrix", "Venezuela", "Chile", "Ecuador"]
 
 export default function Home() {
+  const [keyword, setKeyword] = useState('')
+  const [path, pushLocation] = useLocation()
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    // navegar a otra ruta
+    pushLocation('/search/${keyword}')
+
+  }
+
+  const handleChange = evt => {
+    setKeyword(evt.target.value)
+  }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Search a gif here...." onChange={handleChange} type='text' value={keyword} />
+      </form>
       <h3 className="App-title">Los gifs más populares</h3>
       <ul>
         {POPULAR_GIFS.map((popularGif) => (
