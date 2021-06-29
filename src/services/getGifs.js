0f4ace05d27551014c19475a8c4d4be07032280e
Byron 2016 <bgva2005@yahoo.com>
2021-06-29 18:02:52 -0500

    V03P_Sacar respuesta del fetch a función

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,33 +1,40 @@
-import { useState } from "react"
+// import { useState } from "react"
 
 const apiKey = 'Pboj2pSFbOlMbjmKYHNlnwOG7oSSdvtg'
 
-export default function getGifs ({keyword = 'matrix'} = {}) {
-  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+const fromApiResponseToGifs = apiResponse => {
+  const {data = []} = apiResponse
+  if (Array.isArray(data)) {
+    const gifs = data.map(image => {
+      const {images, title, id} = image
+      const { url } = images.downsized_medium
+      return { title, id, url }
+    })
+    return gifs
+  }
+  return []
+}
+
+export default function getGifs ({limit = 10, keyword = 'morty'} = {}) {
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
   // En caso q fetch no esté soportado por navegador, se puede usar
   // github.com/developit/unfetch
 
   return fetch(apiURL)
   .then((res) => res.json())
-  .then((response) => {
-    const { data = [] } = response
-    if (Array.isArray(data)) {
-      // console.log('Dentro de if del getGif ')
-      // console.log(data)
-      const gifsRetornados = data.map(image => {
-        // console.log('Valor del url: ' + image.images.downsized_medium.url)
-        const {images, title, id} = image
-        const {url} = image.images.downsized_medium.url
-        const urlValor = images.downsized_medium.url
-        // console.log('Valor del url (images): ' + urlValor)
-        // console.log({url})
-        //return {title, id, url}
-        return {title, id, url: urlValor}
+  .then(fromApiResponseToGifs)
+  // .then((response) => {
+  //   const { data = [] } = response
+  //   if (Array.isArray(data)) {
+  //     const gifsRetornados = data.map(image => {
+  //       const {images, title, id} = image
+  //       const {url} = image.images.downsized_medium.url
+  //       const urlValor = images.downsized_medium.url
+  //       //return {title, id, url}
+  //       return {title, id, url: urlValor}
 
-      })
-      // console.log('Dentro de getGif ')
-      // console.log(gifs)
-      return gifsRetornados
-    }
-  })
+  //     })
+  //     return gifsRetornados
+  //   }
+  // })
 }
