09f1be97bfdd0681e2c21af8767e06429ca125da
Byron 2016 <bgva2005@yahoo.com>
2021-06-29 20:11:10 -0500

    V03_Añadiendo un form que haga de buscador de Gifs (7.22)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from "wouter"
 
 const POPULAR_GIFS = ["USA","Matrix", "Chile", "Colombia", "Ecuador"]
 
 export default function Home() {
+  const [keyword, setKeyword] = useState('')
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    //navegar a otra ruta
+    console.log({keyword})
+  }
+
+  const handleChange = evt => {
+    setKeyword(evt.target.value)
+  }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
+      </form>
       <h3 className="App-title">Los gifs más populares</h3>
       <ul>
       {POPULAR_GIFS.map((popularGif) => (
