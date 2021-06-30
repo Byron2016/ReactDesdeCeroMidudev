91a1198b77fa9b3dd3b82aed56e6c21081b24e48
Byron 2016 <bgva2005@yahoo.com>
2021-06-30 09:11:33 -0500

    V03_Recuperar desde Details el detalle del gif del estado global (1.17.06)

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,13 +1,15 @@
 import React,  { useContext } from "react"
 // import StaticContext from '../../context/StaticContext'
 import GifsContext from '../../context/GifsContext'
+import Gif from '../../components/Gif/index'
 
 export default function Detail ({ params }) {
   // const staticConfig = useContext(StaticContext)
   // console.log(staticConfig)
   const gifsContext = useContext(GifsContext)
   const {gifs} = useContext(GifsContext)
+  const gif = gifs.find(singleGif => singleGif.id === params.id)
   console.log(gifs)
   // console.log(params.id)
-  return <h1>GIF con id {params.id}</h1>
+  return <Gif {...gif} />
 }
